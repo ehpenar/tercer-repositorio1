4c7f65db2a184a4ba4be6fcf0807036627c3c490
ehpenar <ehpenar@github.com>
2017-08-18 15:16:14 -0500

    agregamos algunos archivos y funciones

---
+++ tercer-repositorio1/demo.py
@@ -0,0 +1,18 @@
+""" 
+TItulo
+============
+comentario sobre el modulo (se puede poner con formato, es similar al markdown)
+
+"""
+def sum(a,v):
+    """
+    esta es una funcion que recibe dos parametros y retorna la suma de ellos
+    
+    Args:
+         a (float): numero que va a sumar
+         v (float): numero que va a sumar
+    """
+    return a+v
+
+print(sum(1,2))
+
